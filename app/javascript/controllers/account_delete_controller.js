36071ecdc89c2b5f9822b274ddafb72f622f9252
Francois Pazery <asper-01@hotmail.fr>
2025-01-09 12:18:49 +0100

    light fix

--- app/javascript/controllers/account_delete_controller.js
+++ app/javascript/controllers/account_delete_controller.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const confirmDeleteBtn = document.getElementById("confirm-delete-btn");
   const cancelDeleteBtn = document.getElementById("cancel-delete-btn");
   const showDeleteBtn = document.getElementById("show-delete-btn");
   const confirmDeleteSection = document.getElementById("confirm-delete-section");
 
-  if (confirmDeleteBtn && cancelDeleteBtn && showDeleteBtn) {
+  if (cancelDeleteBtn && showDeleteBtn && confirmDeleteSection) {
     // Affiche les boutons de confirmation et d'annulation
     showDeleteBtn.addEventListener("click", () => {
       showDeleteBtn.classList.add("d-none");
