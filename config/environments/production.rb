9161d69b86e581b49a7b1d59401b2e5403e93b98
Francois Pazery <asper-01@hotmail.fr>
2025-01-31 17:05:28 +0100

    assets precompile false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.digest = true
   config.assets.css_compressor = :scss
   config.assets.precompile += %w( application.js application.css )
