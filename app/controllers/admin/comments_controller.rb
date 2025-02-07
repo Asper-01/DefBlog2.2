33b019c7a441a5a6f42135b09c73b311e3d51e81
Francois Pazery <asper-01@hotmail.fr>
2025-02-07 23:58:47 +0100

    controllers fix

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,30 +1,32 @@
-class CommentsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_article
+module Admin
+  class CommentsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_article
 
-  def create
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.new(comment_params)
-    @comment.user = current_user
+    def create
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.new(comment_params)
+      @comment.user = current_user
 
-    if @comment.save
-      redirect_to article_path(@article), notice: 'Commentaire ajouté avec succès.'
-    else
-      @comments = @article.comments.order(created_at: :desc) # Nécessaire pour afficher les commentaires existants
-      render 'articles/show' # Rester sur la page actuelle avec les erreurs
+      if @comment.save
+        redirect_to article_path(@article), notice: 'Commentaire ajouté avec succès.'
+      else
+        @comments = @article.comments.order(created_at: :desc) # Nécessaire pour afficher les commentaires existants
+        render 'articles/show' # Rester sur la page actuelle avec les erreurs
+      end
     end
-  end
 
 
 
-  private
+    private
 
-  def set_article
-    @article = Article.find(params[:article_id])
-  end
+    def set_article
+      @article = Article.find(params[:article_id])
+    end
 
-  def comment_params
-    Rails.logger.debug "Params reçus : #{params.inspect}"
-    params.require(:comment).permit(:content, :article_id, :user_id, :created_at, :updated_at, :user, :article, :id)
+    def comment_params
+      Rails.logger.debug "Params reçus : #{params.inspect}"
+      params.require(:comment).permit(:content, :article_id, :user_id, :created_at, :updated_at, :user, :article, :id)
+    end
   end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,48 +1,50 @@
-class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters, if: :devise_controller?
+module Users
+  class Users::RegistrationsController < Devise::RegistrationsController
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # PUT /resource
-  def update
-    # Gérer l'avatar s'il est présent ou s'il doit être supprimé
-    if params[:user][:remove_avatar] == "1"
-      resource.avatar.detach
+    # PUT /resource
+    def update
+      # Gérer l'avatar s'il est présent ou s'il doit être supprimé
+      if params[:user][:remove_avatar] == "1"
+        resource.avatar.detach
+      end
+
+      if update_resource(resource, account_update_params)
+        set_flash_message(:notice, :updated) if is_flashing_format?
+        bypass_sign_in(resource, scope: resource_name) # Maintenir la session après mise à jour
+        redirect_to after_update_path_for(resource), notice: "Votre profil a été mis à jour avec succès."
+      else
+        clean_up_passwords(resource)
+        set_minimum_password_length
+        render :edit
+      end
     end
 
-    if update_resource(resource, account_update_params)
-      set_flash_message(:notice, :updated) if is_flashing_format?
-      bypass_sign_in(resource, scope: resource_name) # Maintenir la session après mise à jour
-      redirect_to after_update_path_for(resource), notice: "Votre profil a été mis à jour avec succès."
-    else
-      clean_up_passwords(resource)
-      set_minimum_password_length
-      render :edit
+    protected
+
+    # Permettre des paramètres supplémentaires pour l'inscription et la mise à jour de l'utilisateur
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [
+        :avatar, :remove_avatar, :name, :email, :cookies_consent,
+        :password, :password_confirmation, :current_password
+      ])
     end
+
+    # Autoriser des paramètres pour la mise à jour
+    def account_update_params
+      params.require(:user).permit(
+        :name, :email, :avatar, :remove_avatar, :cookies_consent,
+        :password, :password_confirmation, :current_password
+      )
+    end
+
+    # Permet de mettre à jour les informations de l'utilisateur sans exiger le mot de passe
+    def update_resource(resource, params)
+      # Supprimer le champ `current_password` si présent
+      params.delete(:current_password)
+      resource.update_without_password(params)
+    end
+
   end
-
-  protected
-
-  # Permettre des paramètres supplémentaires pour l'inscription et la mise à jour de l'utilisateur
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [
-      :avatar, :remove_avatar, :name, :email, :cookies_consent,
-      :password, :password_confirmation, :current_password
-    ])
-  end
-
-  # Autoriser des paramètres pour la mise à jour
-  def account_update_params
-    params.require(:user).permit(
-      :name, :email, :avatar, :remove_avatar, :cookies_consent,
-      :password, :password_confirmation, :current_password
-    )
-  end
-
-  # Permet de mettre à jour les informations de l'utilisateur sans exiger le mot de passe
-  def update_resource(resource, params)
-    # Supprimer le champ `current_password` si présent
-    params.delete(:current_password)
-    resource.update_without_password(params)
-  end
-
 end
